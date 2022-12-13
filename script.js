d1eb002e4f73b47d0672ed4814b5503e3a001f58
nah <hef9874@gmail.com>
2022-12-12 21:04:39 -0500

    moved html, css, and js out of develop folder

--- script.js
+++ script.js
@@ -78,4 +78,4 @@ function writePassword() {
 }
   
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
